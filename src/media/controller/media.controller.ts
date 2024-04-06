6ece39e21ca41cf75dd25dc18877b3482f441f25
devBjn <tamnguyen.10092000tv@gmail.com>
2024-04-06 17:07:53 +0700

    api public media

--- src/media/controller/media.controller.ts
+++ src/media/controller/media.controller.ts
@@ -1,3 +1,28 @@
-import { Controller } from '@nestjs/common';
-@Controller('meidas')
-export class MediaController {}
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { MediaService } from '../media.service';
+import { AuthGuardJwt } from 'src/auth/auth-guard.jwt';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { FilesInterceptor } from '@nestjs/platform-express';
+
+@ApiTags('Media')
+@Controller('medias')
+export class MediaController {
+  constructor(private readonly mediaService: MediaService) {}
+
+  @Post('upload')
+  @UseGuards(AuthGuardJwt)
+  @ApiBearerAuth()
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
+    const data = Promise.all(
+      files.map(async (file) => await this.mediaService.upload(file)),
+    );
+    return data;
+  }
+}
